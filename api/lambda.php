85f5fcb05cdf1e6ea2fceebd05f5618510937bdd
Mazen Hamed <mazen@Mazens-MacBook-Air.local>
2026-03-27 10:46:33 +0800

    fix: fixed vercel deploy v5

--- api/lambda.php
+++ api/lambda.php
@@ -1,20 +1,35 @@
 <?php
-use Illuminate\Http\Request;
+
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-
-
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-
 /** @var Application $app */
+if (getenv('VERCEL')) {
+    $storage = '/tmp/gk-laravel-storage';
+    foreach ([
+        'framework',
+        'framework/cache',
+        'framework/cache/data',
+        'framework/sessions',
+        'framework/views',
+        'logs',
+    ] as $dir) {
+        $path = $storage.'/'.$dir;
+        if (! is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+    }
+    $app->useStoragePath($storage);
+}
+
 $app->handleRequest(Request::capture());
